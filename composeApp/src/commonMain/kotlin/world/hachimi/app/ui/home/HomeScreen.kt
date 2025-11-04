21ae3ebf1778b2c1aceac926a26956db9486f071
KonYaco <atwzj233@gmail.com>
2025-11-04 20:35:45 +0800

    [feat](home) Support hot songs and pure category in HomeScreen

--- composeApp/src/commonMain/kotlin/world/hachimi/app/ui/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/world/hachimi/app/ui/home/HomeScreen.kt
@@ -1,6 +1,12 @@
 package world.hachimi.app.ui.home
 
 import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
@@ -13,8 +19,10 @@ import androidx.compose.material3.*
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import org.koin.compose.koinInject
@@ -28,6 +36,7 @@ import world.hachimi.app.ui.component.LoadingPage
 import world.hachimi.app.ui.component.ReloadPage
 import world.hachimi.app.ui.home.components.SongCard
 import world.hachimi.app.util.WindowSize
+import kotlin.collections.emptyList
 import kotlin.time.Duration.Companion.seconds
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -63,9 +72,7 @@ fun HomeMainScreen(
                 SegmentHeader("每日推荐", onMoreClick = {
                     global.nav.push(Route.Root.Home.Recommend)
                 })
-
                 Spacer(Modifier.height(24.dp))
-
                 LoadableContent(
                     modifier = Modifier.fillMaxWidth().height(520.dp),
                     initializeStatus = vm.recommendStatus,
@@ -106,9 +113,7 @@ fun HomeMainScreen(
                 SegmentHeader("最近发布", onMoreClick = {
                     global.nav.push(Route.Root.Home.Recent)
                 })
-
                 Spacer(Modifier.height(24.dp))
-
                 LoadableContent(
                     modifier = Modifier.fillMaxWidth().height(520.dp),
                     initializeStatus = vm.recentStatus,
@@ -149,28 +154,90 @@ fun HomeMainScreen(
                 SegmentHeader("本周热门", onMoreClick = {
                     global.nav.push(Route.Root.Home.WeeklyTop)
                 })
-
                 Spacer(Modifier.height(24.dp))
+                LoadableContent(
+                    modifier = Modifier.fillMaxWidth().height(520.dp),
+                    initializeStatus = vm.hotStatus,
+                    loading = vm.hotLoading,
+                    onRefresh = { },
+                    onRetryClick = { vm.retryHot() }
+                ) {
+                    if (vm.hotSongs.isEmpty()) Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        Text("空空如也")
+                    } else LazyHorizontalGrid(
+                        modifier = Modifier.fillMaxSize(),
+                        rows = GridCells.Fixed(2),
+                        contentPadding = PaddingValues(start = 24.dp, end = 24.dp, bottom = 24.dp),
+                        horizontalArrangement = Arrangement.spacedBy(24.dp),
+                        verticalArrangement = Arrangement.spacedBy(24.dp)
+                    ) {
+                        items(vm.hotSongs, key = { item -> item.id }) { item ->
+                            SongCard(
+                                modifier = Modifier.width(width = 180.dp),
+                                item = item,
+                                onClick = {
+                                    global.player.insertToQueue(
+                                        GlobalStore.MusicQueueItem(
+                                            id = item.id,
+                                            displayId = item.displayId,
+                                            name = item.title,
+                                            artist = item.uploaderName,
+                                            duration = item.durationSeconds.seconds,
+                                            coverUrl = item.coverUrl
+                                        ), true, false
+                                    )
+                                },
+                            )
+                        }
+                    }
+                }
 
-                SegmentHeader("小众宝藏", onMoreClick = {
+                SegmentHeader("纯净哈基米专区", onMoreClick = {
                     global.nav.push(Route.Root.Home.HiddenGem)
                 })
                 Spacer(Modifier.height(24.dp))
-
-
-                SegmentHeader("纯净哈基米", onMoreClick = {
-                    global.nav.push(Route.Root.Home.HiddenGem)
-                })
-                Spacer(Modifier.height(24.dp))
-
-                SegmentHeader("R&B 专区", onMoreClick = {
-                    global.nav.push(Route.Root.Home.HiddenGem)
-                })
-                Spacer(Modifier.height(24.dp))
-                SegmentHeader("R&B 专区", onMoreClick = {
-                    global.nav.push(Route.Root.Home.HiddenGem)
-                })
-                Spacer(Modifier.height(24.dp))
+                LoadableContent(
+                    modifier = Modifier.fillMaxWidth().height(520.dp),
+                    initializeStatus = vm.pureStatus,
+                    loading = vm.pureLoading,
+                    onRefresh = { },
+                    onRetryClick = { vm.retryPure() }
+                ) {
+                    if (vm.pureSongs.isEmpty()) Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        Text("空空如也")
+                    } else LazyHorizontalGrid(
+                        modifier = Modifier.fillMaxSize(),
+                        rows = GridCells.Fixed(2),
+                        contentPadding = PaddingValues(start = 24.dp, end = 24.dp, bottom = 24.dp),
+                        horizontalArrangement = Arrangement.spacedBy(24.dp),
+                        verticalArrangement = Arrangement.spacedBy(24.dp)
+                    ) {
+                        items(vm.pureSongs, key = { item -> item.id }) { item ->
+                            SongCard(
+                                modifier = Modifier.width(width = 180.dp),
+                                coverUrl = item.coverArtUrl,
+                                title = item.title,
+                                subtitle = item.subtitle,
+                                author = item.uploaderName,
+                                tags = remember { emptyList<String>() },
+                                playCount = item.playCount,
+                                likeCount = item.likeCount,
+                                onClick = {
+                                    global.player.insertToQueue(
+                                        GlobalStore.MusicQueueItem(
+                                            id = item.id,
+                                            displayId = item.displayId,
+                                            name = item.title,
+                                            artist = item.uploaderName,
+                                            duration = item.durationSeconds.seconds,
+                                            coverUrl = item.coverArtUrl
+                                        ), true, false
+                                    )
+                                },
+                            )
+                        }
+                    }
+                }
             }
         }
     }
@@ -218,7 +285,20 @@ private fun LoadableContent(
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit,
 ) {
-    AnimatedContent(initializeStatus, modifier = modifier) {
+    val slideOffset = with(LocalDensity.current) {
+        32.dp.roundToPx()
+    }
+    AnimatedContent(initializeStatus, modifier = modifier, transitionSpec = {
+        if (targetState == InitializeStatus.LOADED) {
+            (fadeIn(animationSpec = tween(220, delayMillis = 90)) +
+                    slideInVertically(initialOffsetY = { slideOffset }, animationSpec = tween(220, delayMillis = 90)))
+                .togetherWith(fadeOut(animationSpec = tween(90)))
+        } else {
+            (fadeIn(animationSpec = tween(220, delayMillis = 90)) +
+                    scaleIn(initialScale = 0.92f, animationSpec = tween(220, delayMillis = 90)))
+                .togetherWith(fadeOut(animationSpec = tween(90)))
+        }
+    }) {
         when (it) {
             InitializeStatus.INIT -> LoadingPage()
             InitializeStatus.FAILED -> ReloadPage(onRetryClick)
