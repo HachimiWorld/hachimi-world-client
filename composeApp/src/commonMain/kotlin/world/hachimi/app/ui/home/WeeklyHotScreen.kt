6283c755843ed0563ad00d624b10c900d153a4a8
KonYaco <atwzj233@gmail.com>
2025-11-07 22:04:04 +0800

    [feat](hot) Add retry logic for initialization in WeeklyHotViewModel

--- composeApp/src/commonMain/kotlin/world/hachimi/app/ui/home/WeeklyHotScreen.kt
+++ composeApp/src/commonMain/kotlin/world/hachimi/app/ui/home/WeeklyHotScreen.kt
@@ -39,7 +39,7 @@ fun WeeklyHotScreen(
     AnimatedContent(vm.initializeStatus, modifier = Modifier.fillMaxSize()) {
         when (it) {
             InitializeStatus.INIT -> LoadingPage()
-            InitializeStatus.FAILED -> ReloadPage(onReloadClick = { })
+            InitializeStatus.FAILED -> ReloadPage(onReloadClick = { vm.retry() })
             InitializeStatus.LOADED -> Content(vm, global)
         }
     }
