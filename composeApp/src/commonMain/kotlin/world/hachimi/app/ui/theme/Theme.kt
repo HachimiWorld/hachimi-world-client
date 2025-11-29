6dc418989f759da290ceaceed9849c5aa045c8f0
KonYaco <atwzj233@gmail.com>
2025-11-29 21:02:06 +0800

    [feat](design) Apply HachimiTheme in AppTheme

--- composeApp/src/commonMain/kotlin/world/hachimi/app/ui/theme/Theme.kt
+++ composeApp/src/commonMain/kotlin/world/hachimi/app/ui/theme/Theme.kt
@@ -1,20 +1,14 @@
 package world.hachimi.app.ui.theme
-/*import android.app.Activity
-import android.os.Build*/
-/*import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.Typography*/
+
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Immutable
-import androidx.compose.ui.graphics.Color
-
-/*import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext*/
+import world.hachimi.app.ui.design.HachimiTheme
+import world.hachimi.app.ui.design.hachimiDarkScheme
+import world.hachimi.app.ui.design.hachimiLightScheme
 
 private val lightScheme = lightColorScheme(
     primary = primaryLight,
@@ -244,42 +238,29 @@ private val highContrastDarkColorScheme = darkColorScheme(
     surfaceContainerHighest = surfaceContainerHighestDarkHighContrast,
 )
 
-@Immutable
-data class ColorFamily(
-    val color: Color,
-    val onColor: Color,
-    val colorContainer: Color,
-    val onColorContainer: Color
-)
-
-val unspecified_scheme = ColorFamily(
-    Color.Unspecified, Color.Unspecified, Color.Unspecified, Color.Unspecified
-)
 
 @Composable
 fun AppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = false,
-    content: @Composable() () -> Unit
+    content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        /*      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-                  val context = LocalContext.current
-                  if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-              }*/
-
+    val hachimiColorScheme = if (darkTheme) hachimiDarkScheme else hachimiLightScheme
+    val mdColorScheme = when {
         darkTheme -> darkScheme
         else -> lightScheme
     }
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = AppTypography,
-        content = {
-            SystemAppearance(darkTheme)
-            content()
-        }
-    )
+    HachimiTheme(
+        colorScheme = hachimiColorScheme
+    ) {
+        MaterialTheme(
+            colorScheme = mdColorScheme,
+            typography = AppTypography,
+            content = {
+                SystemAppearance(darkTheme)
+                content()
+            }
+        )
+    }
 }
 
 
@@ -289,22 +270,13 @@ fun PreviewTheme(
     background: Boolean,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        darkTheme -> darkScheme
-        else -> lightScheme
+    AppTheme(darkTheme) {
+        if (background) {
+            Surface(color = MaterialTheme.colorScheme.background, content = content)
+        } else {
+            content()
+        }
     }
-
-    if (background) MaterialTheme(
-        colorScheme = colorScheme,
-        typography = AppTypography
-    ) {
-        Surface(color = MaterialTheme.colorScheme.background, content = content)
-    }
-    else MaterialTheme(
-        colorScheme = colorScheme,
-        typography = AppTypography,
-        content = content
-    )
 }
 
 @Composable
