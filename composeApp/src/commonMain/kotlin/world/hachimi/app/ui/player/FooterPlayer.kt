ac22edbf25346ec16daefd906356dd432dfc2e92
KonYaco <atwzj233@gmail.com>
2025-12-09 23:44:31 +0800

    [refactor](player) Extract `AddToPlaylistDialo` and `CreatePlaylistDialog`

--- composeApp/src/commonMain/kotlin/world/hachimi/app/ui/player/FooterPlayer.kt
+++ composeApp/src/commonMain/kotlin/world/hachimi/app/ui/player/FooterPlayer.kt
@@ -4,13 +4,9 @@ import androidx.compose.animation.*
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.PlaylistAdd
 import androidx.compose.material.icons.automirrored.filled.QueueMusic
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Explicit
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -25,19 +21,14 @@ import androidx.compose.ui.window.Popup
 import androidx.compose.ui.window.PopupProperties
 import coil3.compose.AsyncImage
 import org.koin.compose.koinInject
-import org.koin.compose.viewmodel.koinViewModel
 import world.hachimi.app.model.GlobalStore
-import world.hachimi.app.model.PlaylistViewModel
 import world.hachimi.app.ui.LocalAnimatedVisibilityScope
 import world.hachimi.app.ui.LocalSharedTransitionScope
 import world.hachimi.app.ui.SharedTransitionKeys
 import world.hachimi.app.ui.insets.currentSafeAreaInsets
-import world.hachimi.app.ui.player.components.SongControl
-import world.hachimi.app.ui.player.components.SongProgress
-import world.hachimi.app.ui.player.components.VolumeControl
+import world.hachimi.app.ui.player.components.*
 import world.hachimi.app.ui.root.component.MusicQueue
 import world.hachimi.app.util.WindowSize
-import world.hachimi.app.util.singleLined
 import kotlin.random.Random
 
 @Composable
@@ -360,160 +351,4 @@ private fun Cover(global: GlobalStore, displayedCover: String?) {
             )
         }
     }
-}
-
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-private fun AddToPlaylistDialog(
-    tobeAddedSongId: Long?,
-    random: Long?,
-    vm: PlaylistViewModel = koinViewModel(),
-) {
-    LaunchedEffect(vm, tobeAddedSongId, random) {
-        if (tobeAddedSongId != null) {
-            vm.toBeAddedSongId = tobeAddedSongId
-            vm.addToPlaylist()
-        }
-    }
-
-    if (vm.showPlaylistDialog) BasicAlertDialog(
-        modifier = Modifier.width(400.dp),
-        onDismissRequest = {
-            vm.cancelAddToPlaylist()
-        },
-        content = {
-            ElevatedCard(elevation = CardDefaults.elevatedCardElevation(defaultElevation = 8.dp)) {
-                Column(Modifier.padding(24.dp)) {
-                    Text("收藏到歌单", style = MaterialTheme.typography.titleLarge)
-
-                    Spacer(Modifier.height(16.dp))
-
-                    Text("请选择一个歌单")
-
-                    if (vm.playlistIsLoading) {
-                        LinearProgressIndicator()
-                    }
-
-                    Spacer(Modifier.height(12.dp))
-
-                    Card(
-                        modifier = Modifier.fillMaxWidth(),
-                        onClick = { vm.createPlaylist() }
-                    ) {
-                        Row(Modifier.padding(16.dp)) {
-                            Icon(Icons.Default.Add, contentDescription = null)
-                            Text(
-                                modifier = Modifier.padding(start = 16.dp),
-                                text = "新建歌单"
-                            )
-                        }
-                    }
-
-                    Spacer(Modifier.height(12.dp))
-
-                    LazyColumn(Modifier.height(200.dp).fillMaxWidth()) {
-                        itemsIndexed(vm.playlists, key = { _, item -> item.id }) { index, item ->
-                            Card(
-                                modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
-                                onClick = {
-                                    vm.selectedPlaylistId = item.id
-                                }
-                            ) {
-                                Row(
-                                    modifier = Modifier,
-                                    verticalAlignment = Alignment.CenterVertically
-                                ) {
-                                    Surface(
-                                        Modifier.size(64.dp),
-                                        MaterialTheme.shapes.medium,
-                                        LocalContentColor.current.copy(0.12f)
-                                    ) {
-                                        AsyncImage(
-                                            modifier = Modifier.fillMaxSize(),
-                                            model = item.coverUrl,
-                                            contentDescription = "Playlist Cover",
-                                            contentScale = ContentScale.Crop,
-                                        )
-                                    }
-                                    Spacer(Modifier.width(16.dp))
-                                    Text(
-                                        modifier = Modifier.weight(1f),
-                                        text = item.name
-                                    )
-                                    if (vm.selectedPlaylistId == item.id) {
-                                        Icon(
-                                            modifier = Modifier.padding(end = 12.dp),
-                                            imageVector = Icons.Default.CheckCircle,
-                                            contentDescription = "Selected"
-                                        )
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                    Row(Modifier.align(Alignment.End)) {
-                        TextButton(onClick = { vm.cancelAddToPlaylist() }) {
-                            Text("取消")
-                        }
-                        TextButton(
-                            onClick = { vm.confirmAddToPlaylist() },
-                            enabled = vm.selectedPlaylistId != null && !vm.addingToPlaylistOperating
-                        ) {
-                            Text("确定")
-                        }
-                    }
-                }
-            }
-        }
-    )
-}
-
-@Composable
-private fun CreatePlaylistDialog(vm: PlaylistViewModel = koinViewModel()) {
-    if (vm.showCreatePlaylistDialog) AlertDialog(
-        title = { Text("新建歌单") },
-        text = {
-            Column(verticalArrangement = Arrangement.spacedBy(12.dp)) {
-                TextField(
-                    value = vm.createPlaylistName,
-                    onValueChange = { vm.createPlaylistName = it.singleLined() },
-                    label = { Text("名称") },
-                    singleLine = true
-                )
-                TextField(
-                    value = vm.createPlaylistDescription,
-                    onValueChange = { vm.createPlaylistDescription = it },
-                    label = { Text("描述") },
-                    minLines = 3,
-                    maxLines = 3
-                )
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Text("私有歌单")
-                    Switch(
-                        modifier = Modifier.padding(start = 16.dp),
-                        checked = vm.createPlaylistPrivate,
-                        onCheckedChange = { vm.createPlaylistPrivate = it }
-                    )
-                }
-            }
-        },
-        confirmButton = {
-            TextButton(
-                onClick = { vm.confirmCreatePlaylist() },
-                enabled = vm.createPlaylistName.isNotBlank() && !vm.createPlaylistOperating
-            ) {
-                Text("创建")
-            }
-        },
-        dismissButton = {
-            TextButton(onClick = { vm.cancelCreatePlaylist() }) {
-                Text("取消")
-            }
-        },
-        onDismissRequest = {
-            vm.cancelCreatePlaylist()
-        }
-    )
 }
---
+++ composeApp/src/commonMain/kotlin/world/hachimi/app/ui/player/components/AddToPlaylistDialog.kt
@@ -0,0 +1,125 @@
+package world.hachimi.app.ui.player.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import org.koin.compose.viewmodel.koinViewModel
+import world.hachimi.app.model.PlaylistViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddToPlaylistDialog(
+    tobeAddedSongId: Long?,
+    random: Long?,
+    vm: PlaylistViewModel = koinViewModel(),
+) {
+    LaunchedEffect(vm, tobeAddedSongId, random) {
+        if (tobeAddedSongId != null) {
+            vm.toBeAddedSongId = tobeAddedSongId
+            vm.addToPlaylist()
+        }
+    }
+
+    if (vm.showPlaylistDialog) BasicAlertDialog(
+        modifier = Modifier.width(400.dp),
+        onDismissRequest = {
+            vm.cancelAddToPlaylist()
+        },
+        content = {
+            ElevatedCard(elevation = CardDefaults.elevatedCardElevation(defaultElevation = 8.dp)) {
+                Column(Modifier.padding(24.dp)) {
+                    Text("收藏到歌单", style = MaterialTheme.typography.titleLarge)
+
+                    Spacer(Modifier.height(16.dp))
+
+                    Text("请选择一个歌单")
+
+                    if (vm.playlistIsLoading) {
+                        LinearProgressIndicator()
+                    }
+
+                    Spacer(Modifier.height(12.dp))
+
+                    Card(
+                        modifier = Modifier.fillMaxWidth(),
+                        onClick = { vm.createPlaylist() }
+                    ) {
+                        Row(Modifier.padding(16.dp)) {
+                            Icon(Icons.Default.Add, contentDescription = null)
+                            Text(
+                                modifier = Modifier.padding(start = 16.dp),
+                                text = "新建歌单"
+                            )
+                        }
+                    }
+
+                    Spacer(Modifier.height(12.dp))
+
+                    LazyColumn(Modifier.height(200.dp).fillMaxWidth()) {
+                        itemsIndexed(vm.playlists, key = { _, item -> item.id }) { index, item ->
+                            Card(
+                                modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
+                                onClick = {
+                                    vm.selectedPlaylistId = item.id
+                                }
+                            ) {
+                                Row(
+                                    modifier = Modifier,
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    Surface(
+                                        Modifier.size(64.dp),
+                                        MaterialTheme.shapes.medium,
+                                        LocalContentColor.current.copy(0.12f)
+                                    ) {
+                                        AsyncImage(
+                                            modifier = Modifier.fillMaxSize(),
+                                            model = item.coverUrl,
+                                            contentDescription = "Playlist Cover",
+                                            contentScale = ContentScale.Crop,
+                                        )
+                                    }
+                                    Spacer(Modifier.width(16.dp))
+                                    Text(
+                                        modifier = Modifier.weight(1f),
+                                        text = item.name
+                                    )
+                                    if (vm.selectedPlaylistId == item.id) {
+                                        Icon(
+                                            modifier = Modifier.padding(end = 12.dp),
+                                            imageVector = Icons.Default.CheckCircle,
+                                            contentDescription = "Selected"
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    Row(Modifier.align(Alignment.End)) {
+                        TextButton(onClick = { vm.cancelAddToPlaylist() }) {
+                            Text("取消")
+                        }
+                        TextButton(
+                            onClick = { vm.confirmAddToPlaylist() },
+                            enabled = vm.selectedPlaylistId != null && !vm.addingToPlaylistOperating
+                        ) {
+                            Text("确定")
+                        }
+                    }
+                }
+            }
+        }
+    )
+}
