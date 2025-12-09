fb009bae2017fd159fea04087e7a1d0b764e3e03
KonYaco <atwzj233@gmail.com>
2025-12-09 23:36:06 +0800

    [feat](player) Add marquee to footer player

--- composeApp/src/commonMain/kotlin/world/hachimi/app/ui/player/footer/components/Title.kt
+++ composeApp/src/commonMain/kotlin/world/hachimi/app/ui/player/footer/components/Title.kt
@@ -1,9 +1,11 @@
 package world.hachimi.app.ui.player.footer.components
 
+import androidx.compose.foundation.basicMarquee
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import world.hachimi.app.ui.design.HachimiTheme
 import world.hachimi.app.ui.design.components.Text
@@ -19,10 +21,10 @@ fun Title(
     text: String
 ) {
     Text(
+        modifier = Modifier.basicMarquee(iterations = Int.MAX_VALUE, velocity = 10.dp),
         text = text,
         style = titleTypography,
         color = HachimiTheme.colorScheme.onSurface,
-        overflow = TextOverflow.Ellipsis,
         maxLines = 1,
     )
 }
