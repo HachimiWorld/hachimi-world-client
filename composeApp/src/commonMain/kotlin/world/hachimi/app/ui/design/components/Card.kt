79e1356838738b14ad894ffb22f7e1e5cc7cab04
KonYaco <atwzj233@gmail.com>
2025-12-10 14:20:57 +0800

    [feat](design) Add Card component

---
+++ composeApp/src/commonMain/kotlin/world/hachimi/app/ui/design/components/Card.kt
@@ -0,0 +1,54 @@
+package world.hachimi.app.ui.design.components
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.dropShadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.shadow.Shadow
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.dp
+import dev.chrisbanes.haze.HazeState
+import dev.chrisbanes.haze.HazeStyle
+import dev.chrisbanes.haze.HazeTint
+import dev.chrisbanes.haze.hazeEffect
+import world.hachimi.app.ui.design.HachimiTheme
+
+@Composable
+fun Card(
+    hazeState: HazeState,
+    modifier: Modifier = Modifier,
+    shape: RoundedCornerShape = RoundedCornerShape(24.dp),
+    onClick: (() -> Unit)? = null,
+    contentColor: Color = HachimiTheme.colorScheme.onSurface,
+    content: @Composable BoxScope.() -> Unit
+) {
+    CompositionLocalProvider(LocalContentColor provides contentColor) {
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .border(1.dp, HachimiTheme.colorScheme.outline, shape)
+                .dropShadow(
+                    shape = shape,
+                    shadow = Shadow(color = Color.Black.copy(0.06f), radius = 16.dp, spread = 0.dp),
+                )
+                .clip(shape)
+                .hazeEffect(
+                    hazeState, style = HazeStyle(
+                        backgroundColor = HachimiTheme.colorScheme.surface.copy(1f),
+                        blurRadius = 80.dp,
+                        tint = HazeTint(HachimiTheme.colorScheme.surface)
+                    )
+                )
+                .then(if (onClick != null) Modifier.clickable(onClick = onClick) else Modifier.pointerInput(Unit) {}),
+            content = content
+        )
+    }
+}
