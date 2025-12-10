a19d8dc50ec3770be6c90829a8334b1039f68abf
KonYaco <atwzj233@gmail.com>
2025-12-10 21:13:59 +0800

    [feat](player) Handle system bar theme updates with AnimatedVisibility transitions

--- composeApp/src/commonMain/kotlin/world/hachimi/app/ui/player/PlayerScreen2.kt
+++ composeApp/src/commonMain/kotlin/world/hachimi/app/ui/player/PlayerScreen2.kt
@@ -1,9 +1,11 @@
 package world.hachimi.app.ui.player
 
+import androidx.compose.animation.EnterExitState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import org.koin.compose.koinInject
 import world.hachimi.app.model.GlobalStore
+import world.hachimi.app.ui.LocalAnimatedVisibilityScope
 import world.hachimi.app.ui.LocalWindowSize
 import world.hachimi.app.ui.player.fullscreen.CompactPlayerScreen2
 import world.hachimi.app.ui.player.fullscreen.ExpandedPlayerScreen2
@@ -20,6 +22,7 @@ fun PlayerScreen2() {
     val (painter, dominantColor) = rememberAsyncPainterAndColor(global.player.playerState.displayedCover)
     val systemUIController = rememberSystemUIController()
     val parentDarkMode = LocalDarkMode.current
+    val animatedVisibility = LocalAnimatedVisibilityScope.current
 
     PlatformBackHandler {
         global.shrinkPlayer()
@@ -31,13 +34,28 @@ fun PlayerScreen2() {
     ) {
         val contentDarkMode = LocalDarkMode.current
 
-        DisposableEffect(systemUIController, contentDarkMode) {
+        LaunchedEffect(animatedVisibility.transition.currentState, animatedVisibility.transition.targetState) {
+            if (
+                animatedVisibility.transition.currentState == EnterExitState.Visible &&
+                animatedVisibility.transition.targetState == EnterExitState.Visible
+            ) {
+                // Entered
+                systemUIController.setSystemBarsTheme(contentDarkMode)
+            } else if (animatedVisibility.transition.targetState == EnterExitState.PostExit) {
+                // Exiting
+                systemUIController.setSystemBarsTheme(parentDarkMode)
+            }
+        }
+
+        // The code below is useful when AnimatedVisibilityScope is not available
+        // But this screen requires AnimatedVisibilityScope
+        /*DisposableEffect(systemUIController, contentDarkMode) {
             systemUIController.setSystemBarsTheme(contentDarkMode)
             onDispose {
                 // Player exit, restore the dark mode settings
                 systemUIController.setSystemBarsTheme(parentDarkMode)
             }
-        }
+        }*/
         if (LocalWindowSize.current.width < WindowSize.MEDIUM) CompactPlayerScreen2()
         else ExpandedPlayerScreen2()
     }
