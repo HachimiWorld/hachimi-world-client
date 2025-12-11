f4a385570b5540814f350726bdcccafee48d5b8f
KonYaco <atwzj233@gmail.com>
2025-12-11 21:53:52 +0800

    [feat](ui) Update SearchBox

--- composeApp/src/commonMain/kotlin/world/hachimi/app/ui/root/component/SearchBox.kt
+++ composeApp/src/commonMain/kotlin/world/hachimi/app/ui/root/component/SearchBox.kt
@@ -2,6 +2,7 @@ package world.hachimi.app.ui.root.component
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -9,10 +10,13 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import world.hachimi.app.ui.design.components.HachimiIconButton
 import world.hachimi.app.ui.design.components.Icon
 import world.hachimi.app.ui.design.components.Text
@@ -32,8 +36,14 @@ fun SearchBox(
         modifier = modifier,
         value = searchText,
         onValueChange = { onSearchTextChange(it.singleLined()) },
+        shape = RoundedCornerShape(24.dp),
+        textStyle = TextStyle(
+            fontWeight = FontWeight.Medium,
+            fontSize = 16.sp,
+            lineHeight = 24.sp
+        ),
         contentPadding = PaddingValues(start = 8.dp, end = 12.dp, top = 4.dp, bottom = 4.dp),
-        leadingIcon = {
+        prefix = {
             HachimiIconButton(
                 modifier = Modifier.padding(end = 8.dp),
                 touchMode = true,
