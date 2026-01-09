378902cc7c3ae7817b5b7e0c0dfc60234295d584
KonYaco <atwzj233@gmail.com>
2026-01-09 17:45:55 +0800

    [refactor](playlist) Refactor playlist EditDialog. Decouple EditDialog from ViewModel

--- composeApp/src/commonMain/kotlin/world/hachimi/app/ui/playlist/components/EditDialog.kt
+++ composeApp/src/commonMain/kotlin/world/hachimi/app/ui/playlist/components/EditDialog.kt
@@ -4,38 +4,62 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.*
+import androidx.compose.material3.Switch
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import world.hachimi.app.model.PlaylistDetailViewModel
+import world.hachimi.app.ui.design.components.AlertDialog
 import world.hachimi.app.ui.design.components.Text
+import world.hachimi.app.ui.design.components.TextButton
+import world.hachimi.app.ui.design.components.TextField
+import world.hachimi.app.ui.theme.PreviewTheme
 import world.hachimi.app.util.singleLined
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EditDialog(vm: PlaylistDetailViewModel) {
-    if (vm.showEditDialog) BasicAlertDialog(
-        onDismissRequest = { vm.cancelEdit() }
-    ) {
-        ElevatedCard(elevation = CardDefaults.elevatedCardElevation(defaultElevation = 8.dp)) {
+    if (vm.showEditDialog) EditDialog(
+        name = vm.editName,
+        onNameChange = { vm.editName = it.singleLined() },
+        description = vm.editDescription,
+        onDescriptionChange = { vm.editDescription = it },
+        private = vm.editPrivate,
+        onPrivateChange = { vm.editPrivate = it },
+        onCancelClick = { vm.cancelEdit() },
+        onConfirmClick = { vm.confirmEdit() },
+        processing = vm.editOperating
+    )
+}
 
-            Column(
-                modifier = Modifier.padding(24.dp),
-                verticalArrangement = Arrangement.spacedBy(12.dp)
-            ) {
-                Text(text = "编辑歌单信息", style = MaterialTheme.typography.titleLarge)
+@Composable
+private fun EditDialog(
+    name: String,
+    onNameChange: (String) -> Unit,
+    description: String,
+    onDescriptionChange: (String) -> Unit,
+    private: Boolean,
+    onPrivateChange: (Boolean) -> Unit,
+    onCancelClick: () -> Unit,
+    onConfirmClick: () -> Unit,
+    processing: Boolean
+) {
+    AlertDialog(
+        onDismissRequest = onCancelClick,
+        title = { Text(text = "编辑歌单") },
+        text = {
+            Column(verticalArrangement = Arrangement.spacedBy(12.dp)) {
                 TextField(
-                    value = vm.editName,
-                    onValueChange = { vm.editName = it.singleLined() },
-                    label = { Text("名称") },
+                    value = name,
+                    onValueChange = onNameChange,
+                    placeholder = { Text("名称") },
                     singleLine = true
                 )
                 TextField(
-                    value = vm.editDescription,
-                    onValueChange = { vm.editDescription = it },
-                    label = { Text("描述") },
+                    value = description,
+                    onValueChange = onDescriptionChange,
+                    placeholder = { Text("描述") },
                     minLines = 3,
                     maxLines = 3
                 )
@@ -43,23 +67,42 @@ fun EditDialog(vm: PlaylistDetailViewModel) {
                     Text("私有歌单")
                     Switch(
                         modifier = Modifier.padding(start = 16.dp),
-                        checked = vm.editPrivate,
-                        onCheckedChange = { vm.editPrivate = it }
+                        checked = private,
+                        onCheckedChange = onPrivateChange
                     )
                 }
-
-                Row(Modifier.align(Alignment.End)) {
-                    TextButton(onClick = { vm.cancelEdit() }) {
-                        Text("取消")
-                    }
-                    TextButton(
-                        onClick = { vm.confirmEdit() },
-                        enabled = vm.editName.isNotBlank() && !vm.editOperating
-                    ) {
-                        Text("确定")
-                    }
-                }
+            }
+        },
+        confirmButton = {
+            TextButton(
+                onClick = onConfirmClick,
+                enabled = name.isNotBlank() && !processing
+            ) {
+                Text("确定")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onCancelClick) {
+                Text("取消")
             }
         }
+    )
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    PreviewTheme(background = false) {
+        EditDialog(
+            name = "",
+            onNameChange = { },
+            description = "",
+            onDescriptionChange = { },
+            private = false,
+            onPrivateChange = { },
+            onCancelClick = { },
+            onConfirmClick = { },
+            processing = false
+        )
     }
 }
