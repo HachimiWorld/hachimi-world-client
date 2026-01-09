e00b6bec4cc86db3bf982e51daccd332697e2d70
KonYaco <atwzj233@gmail.com>
2026-01-09 17:18:48 +0800

    [feat](design) Add `supportingText` to TextField component

--- composeApp/src/commonMain/kotlin/world/hachimi/app/ui/design/components/TextField.kt
+++ composeApp/src/commonMain/kotlin/world/hachimi/app/ui/design/components/TextField.kt
@@ -1,7 +1,11 @@
 package world.hachimi.app.ui.design.components
 
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
@@ -12,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -27,6 +32,12 @@ private val textFieldStyle = TextStyle(
     lineHeight = 24.sp
 )
 
+private val supportingTextStyle = TextStyle(
+    fontWeight = FontWeight.Normal,
+    fontSize = 12.sp,
+    lineHeight = 16.sp
+)
+
 @Composable
 fun TextField(
     value: String,
@@ -37,6 +48,7 @@ fun TextField(
     placeholder: @Composable (() -> Unit)? = null,
     prefix: @Composable (() -> Unit)? = null,
     postfix: @Composable (() -> Unit)? = null,
+    supportingText: @Composable (() -> Unit)? = null,
     leadingIcon: @Composable (() -> Unit)? = null,
     trailingIcon: @Composable (() -> Unit)? = null,
     enabled: Boolean = true,
@@ -71,35 +83,58 @@ fun TextField(
         interactionSource = interactionSource,
         cursorBrush = cursorBrush,
         decorationBox = { innerTextField ->
-            Surface(
-                shape = shape,
-                color = HachimiTheme.colorScheme.background
-            ) {
-                Row(
-                    modifier = Modifier.padding(contentPadding),
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    CompositionLocalProvider(
-                        LocalContentColor provides HachimiTheme.colorScheme.onSurfaceVariant,
-                        LocalTextStyle provides textFieldStyle
+            Layout(
+                content = {
+                    Surface(
+                        shape = shape,
+                        color = HachimiTheme.colorScheme.background
                     ) {
-                        prefix?.invoke()
-                        leadingIcon?.let {
-                            Box(Modifier.padding(end = 12.dp)) {
-                                it()
+                        Row(
+                            modifier = Modifier.padding(contentPadding),
+                            verticalAlignment = Alignment.CenterVertically,
+                        ) {
+                            CompositionLocalProvider(
+                                LocalContentColor provides HachimiTheme.colorScheme.onSurfaceVariant,
+                                LocalTextStyle provides textFieldStyle
+                            ) {
+                                prefix?.invoke()
+                                leadingIcon?.let {
+                                    Box(Modifier.padding(end = 12.dp)) {
+                                        it()
+                                    }
+                                }
+                                Box(
+                                    modifier = Modifier.weight(1f),
+                                    contentAlignment = Alignment.CenterStart
+                                ) {
+                                    innerTextField()
+                                    if (value.isEmpty()) placeholder?.invoke()
+                                }
+                                trailingIcon?.let {
+                                    Box(Modifier.padding(start = 12.dp)) {
+                                        it()
+                                    }
+                                }
+                                postfix?.invoke()
                             }
                         }
-                        Box(contentAlignment = Alignment.CenterStart) {
-                            innerTextField() // This won't use LocalContentColor
-                            if (value.isEmpty()) placeholder?.invoke()
-                        }
-                        trailingIcon?.let {
-                            Box(Modifier.padding(start = 12.dp)) {
-                                it()
-                            }
-                        }
-                        postfix?.invoke()
                     }
+                    CompositionLocalProvider(LocalTextStyle provides supportingTextStyle) {
+                        supportingText?.invoke()
+                    }
+                }
+            ) { measureables, constraints ->
+                val field = measureables[0]
+                val supporting = measureables.getOrNull(1)
+                val fieldPlaceable = field.measure(constraints)
+                val supportingPlaceable = supporting?.measure(constraints.copy(
+                    minHeight = 0,
+                    maxWidth = fieldPlaceable.width
+                ))
+
+                layout(width = fieldPlaceable.width, height = fieldPlaceable.height + (supportingPlaceable?.height ?: 0)) {
+                    fieldPlaceable.place(0, 0)
+                    supportingPlaceable?.place(0, fieldPlaceable.height)
                 }
             }
         }
