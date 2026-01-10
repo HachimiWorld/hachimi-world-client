660f461a2d6af94899ad2ee8a65d57e45283bfc5
KonYaco <atwzj233@gmail.com>
2026-01-10 18:24:50 +0800

    [refactor](player) Separate horizontal and vertical padding for cover in `ExpandedPlayerScreen2`

--- composeApp/src/commonMain/kotlin/world/hachimi/app/ui/player/fullscreen/ExpandedPlayerScreen2.kt
+++ composeApp/src/commonMain/kotlin/world/hachimi/app/ui/player/fullscreen/ExpandedPlayerScreen2.kt
@@ -7,16 +7,41 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.hoverable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsHoveredAsState
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.CloseFullscreen
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Pause
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.filled.SkipNext
+import androidx.compose.material.icons.filled.SkipPrevious
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -25,7 +50,11 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.*
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.koin.compose.koinInject
 import world.hachimi.app.model.GlobalStore
 import world.hachimi.app.model.PlayerUIState
@@ -38,7 +67,16 @@ import world.hachimi.app.ui.insets.currentSafeAreaInsets
 import world.hachimi.app.ui.player.components.AddToPlaylistDialog
 import world.hachimi.app.ui.player.components.PlayerProgress
 import world.hachimi.app.ui.player.components.ShareDialog
-import world.hachimi.app.ui.player.fullscreen.components.*
+import world.hachimi.app.ui.player.fullscreen.components.AuthorAndPV
+import world.hachimi.app.ui.player.fullscreen.components.Cover
+import world.hachimi.app.ui.player.fullscreen.components.InfoTabContent
+import world.hachimi.app.ui.player.fullscreen.components.JmidLabel
+import world.hachimi.app.ui.player.fullscreen.components.Lyrics2
+import world.hachimi.app.ui.player.fullscreen.components.MusicQueue
+import world.hachimi.app.ui.player.fullscreen.components.Page
+import world.hachimi.app.ui.player.fullscreen.components.PagerButtons
+import world.hachimi.app.ui.player.fullscreen.components.Titles
+import world.hachimi.app.ui.player.fullscreen.components.rememberTabTransitionSpec
 import world.hachimi.app.ui.util.fadingEdges
 import kotlin.math.roundToInt
 import kotlin.random.Random
@@ -215,7 +253,8 @@ private fun LeftPane(
 ) {
     val minCoverSize = 256.dp
     val maxCoverSize = 600.dp
-    val padding = 172.dp
+    val minHorizonPadding = 172.dp
+    val minVerticalPadding = 300.dp
 
     Layout(modifier = modifier, content = {
         header()
@@ -226,8 +265,11 @@ private fun LeftPane(
         val cover = measurables[1]
         val footer = measurables[2]
 
-        val minEdge = minOf(constraints.maxHeight, constraints.maxWidth)
-        val coverSize = (minEdge - padding.toPx()).coerceIn(minCoverSize.toPx(), maxCoverSize.toPx()).roundToInt()
+        val minEdgeSize = minOf(
+            constraints.maxHeight - minVerticalPadding.toPx(),
+            constraints.maxWidth - minHorizonPadding.toPx()
+        )
+        val coverSize = (minEdgeSize).coerceIn(minCoverSize.toPx(), maxCoverSize.toPx()).roundToInt()
 
         val coverPlaceable = cover.measure(Constraints.fixed(coverSize, coverSize))
         val coverY = (constraints.maxHeight - coverPlaceable.height) / 2
