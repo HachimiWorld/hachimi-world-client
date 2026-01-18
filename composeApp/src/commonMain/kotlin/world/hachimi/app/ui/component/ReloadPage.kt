bc2090c565e66e11883aa9c6382163d2bf729437
KonYaco <atwzj233@gmail.com>
2026-01-19 00:31:10 +0800

    [feat](i18n) i18n ReloadPage

--- composeApp/src/commonMain/kotlin/world/hachimi/app/ui/component/ReloadPage.kt
+++ composeApp/src/commonMain/kotlin/world/hachimi/app/ui/component/ReloadPage.kt
@@ -1,11 +1,19 @@
 package world.hachimi.app.ui.component
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import hachimiworld.composeapp.generated.resources.Res
+import hachimiworld.composeapp.generated.resources.reload_page_message
+import hachimiworld.composeapp.generated.resources.reload_retry
+import org.jetbrains.compose.resources.stringResource
 import world.hachimi.app.ui.design.components.Button
 import world.hachimi.app.ui.design.components.Text
 import world.hachimi.app.ui.theme.PreviewTheme
@@ -18,10 +26,10 @@ fun ReloadPage(
 ) {
     Box(modifier, Alignment.Center) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Text("基米好像去往了天堂")
+            Text(message ?: stringResource(Res.string.reload_page_message))
             Spacer(Modifier.height(12.dp))
             Button(onReloadClick) {
-                Text("重试")
+                Text(stringResource(Res.string.reload_retry))
             }
         }
     }
